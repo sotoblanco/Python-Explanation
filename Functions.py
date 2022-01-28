edb5763a028208c55f3370c8cde0ad6ca7dfd29b
Pastor <sotoblanco263542@gmail.com>
2022-01-28 14:27:42 -0400

    tupple unpacking with functions

--- Functions.py
+++ Functions.py
@@ -24,3 +24,86 @@ result = add_function(3, 6)
 
 
 name_of_function("Pastor")
+
+def say_hello(name = "Default"):
+
+    print(f'Hello {name}')
+
+# print just to display the results
+
+# if we want to store into a variable you need to use return
+
+# you can use print and return in the same function althoug is not common
+
+def sum_var(a, b):
+    print(a+b)
+    return a + b
+
+sum_var(10,20)
+
+# returns boolean expression 
+def even_check(number):
+    return number % 2 == 0
+
+even_check(20)
+
+# return true if any number is even inside a list
+
+def check_even_list(num_list):
+    
+    # return all even numbers
+    
+    even_numbers = []
+    
+
+    for number in num_list:
+        if number % 2 == 0:
+            even_numbers.append(number)
+        else:
+            pass
+    return even_numbers
+
+
+
+check_even_list([1,3,5])
+
+# multiple values return from a function using tupple unpacking
+
+stock_prices = [('APPL', 200), ('GOOGL', 400), ('MSFT', 100)]
+
+for item in stock_prices:
+    print(item)
+
+for ticker, prices in stock_prices:
+    print(ticker)
+
+# check how an increase of 10 % looks like
+
+for ticker, prices in stock_prices:
+    print(prices + prices*0.1)
+
+
+work_hours = [("Abby", 100), ("Billy", 4000), ("Cassie", 800)]
+
+def employee_check(work_hours):
+    
+    current_max = 0
+    employee_of_month = ""
+    
+    for employee, hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+            
+        else:
+            pass
+    return (employee_of_month, current_max)
+
+
+# tupple unpacking from a function using tupple
+name, hours = employee_check(work_hours)
+
+
+
+
+
